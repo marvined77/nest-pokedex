88d4226134803b448d7cc0d436da41011d109304
Marvin Linares <marvin.edd7@gmail.com>
2026-08-31 18:34:44 -0600

    Antes de la tarea

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async excuteSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    const pokemons = data.results;
+
+    pokemons.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+
+      console.log({ name, no });
+    });
+
+    return 'Seed executed';
+  }
+}
